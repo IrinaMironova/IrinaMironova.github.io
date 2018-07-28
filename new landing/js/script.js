42c000461db85893c08ec9189c31abf1b15c2698
Irina <mia.ira9090@gmail.com>
2018-07-28 16:20:52 +0300

    commit new

---
+++ new landing/js/script.js
@@ -0,0 +1,45 @@
+var menu = document.getElementById("menu");
+var toggleMenu = document.getElementById("toggle-menu");
+
+toggleMenu.addEventListener("click", function() {
+  showMenu();
+});
+
+function showMenu() {	
+	menu.classList.toggle("active");
+};
+$('.owl-carousel1').owlCarousel({
+    loop:true,
+    margin: 0,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        850:{
+            items:2
+        },
+        1600: {
+           items:3
+        }
+    }
+});
+$( ".owl-prev>span" ).addClass( "prev" );
+$('.prev').replaceWith('<span aria-label="Previous">Previous</span>');
+$( ".owl-next>span" ).addClass( "next" );
+$('.next').replaceWith('<span aria-label="Next">Next</span>');
+$('.owl-carousel2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText : ["",""],
+    responsive:{
+        0:{
+            items:1
+        },
+        760:{
+            items:2
+        }
+        
+    }
+});
